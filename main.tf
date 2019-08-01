a365b26d06fa5c30c5b2efe473eb9dd5086ba586
devoptimist <sbrown@chef.io>
2019-08-01 22:52:34 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+module "chef_backend_cluster" {
+  source                          = "devoptimist/chef-backend/linux"
+  version                         = "0.0.2"
+  bootstrap_node_ip               = var.bootstrap_node_public_ip
+  peers                           = var.bootstrap_node_private_ip
+  backend_ips                     = var.backend_public_ips
+  frontend_ips                    = var.frontend_public_ips
+  frontend_private_ips            = var.frontend_private_ips
+  frontend_node_count             = var.frontend_node_count
+  ssh_user_name                   = var.ssh_user_name
+  ssh_user_private_key            = var.ssh_user_private_key_path
+  extra_frontend_config           = var.extra_frontend_config
+  postgresql_superuser_password   = var.postgresql_superuser_password
+  postgresql_replication_password = var.postgresql_replication_password
+  etcd_initial_cluster_token      = var.etcd_initial_cluster_token
+  elasticsearch_cluster_name      = var.elasticsearch_cluster_name
+  force_frontend_chef_run         = var.force_frontend_chef_run
+}
---
+++ variables.tf
@@ -0,0 +1,71 @@
+variable "bootstrap_node_public_ip" {
+  description = "the bootstrap nodes public ip address"
+  type = string
+}
+
+variable "bootstrap_node_private_ip" {
+  description = "the bootstrap nodes private ip address"
+  type = string
+}
+
+variable "backend_public_ips" {
+  description = "list of the backend public ip addresses"
+  type = list(string)
+}
+
+variable "frontend_public_ips" {
+  description = "list of the frontend public ip addresses"
+  type = list(string)
+}
+
+variable "frontend_private_ips" {
+  description = "list of the frontend private ip addresses"
+  type = list(string)
+}
+
+variable "frontend_node_count" {
+  description = "number of frontend nodes being created"
+  type = number
+}
+
+variable "ssh_user_name" {
+  description = "user name for ssh access to the cluster"
+  type = string
+}
+
+variable "ssh_user_private_key_path" {
+  description = "path for the ssh key used to access the cluster"
+  type = string
+}
+
+variable "postgresql_superuser_password" {
+  description = "a password string"
+  type = string
+}
+
+variable "postgresql_replication_password" {
+  description = "password string"
+  type = string
+}
+
+variable "etcd_initial_cluster_token" {
+  description = "user created token string"
+  type = string
+}
+
+variable "elasticsearch_cluster_name" {
+  description = "user created name for the elasticsearch cluster"
+  type = string
+}
+
+variable "extra_frontend_config" {
+  description = "Chef Backend will auto generate the configuration for the frontends, this variable is to add extra configuration to that configuration"
+  type    = string
+  default = ""
+}
+
+variable "force_frontend_chef_run" {
+  description = "set to anything other than default to force a rerun of chef on all the cluster nodes"
+  type    = string
+  default = "default"
+}
